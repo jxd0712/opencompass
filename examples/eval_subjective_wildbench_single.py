b726cb13dee28d9ca4465f860a0ce70ef9bf7472
Hoter Young <hoteryoung@163.com>
2025-03-20 16:03:21 +0800

    [Fix] Succeed in testing WildBench with WB-Score metric

--- examples/eval_subjective_wildbench_single.py
+++ examples/eval_subjective_wildbench_single.py
@@ -16,7 +16,9 @@ with read_base():
 
     from opencompass.configs.models.openai.o1_mini_2024_09_12 import models as o1_mini_2024_09_12_model
     from opencompass.configs.models.qwq.lmdeploy_qwq_32b_preview import models as lmdeploy_qwq_32b_preview_model
-    from opencompass.configs.models.deepseek.deepseek_r1_api_aliyun import models as deepseek_r1_api_aliyun_model
+    # from opencompass.configs.models.deepseek.deepseek_r1_api_aliyun import models as deepseek_r1_api_aliyun_model
+    # from opencompass.configs.models.deepseek.deepseek_r1_api_sensetime import models as deepseek_r1_api_sensetime_model
+    from opencompass.configs.models.deepseek.deepseek_r1_api_siliconflow import models as deepseek_r1_api_siliconflow_model
     # from opencompass.configs.models.openai.o3_mini_2025_01_31 import models as o3_mini_2025_01_31_model
 
 from opencompass.models import OpenAI
@@ -35,21 +37,12 @@ api_meta_template = dict(round=[
     dict(role='BOT', api_role='BOT', generate=True),
 ])
 
-for model in deepseek_r1_api_aliyun_model:
-    model['return_reasoning_content'] = True
-    model['pred_postprocessor'] = {
-        'open_hu_eval_*': {
-            'type': 'rm_<think>_before_eval'
-        }
-    }
-del model
-
 models = [
     *gpt_4o_mini_20240718_model,
     *gpt_4o_20241120_model,
     *o1_mini_2024_09_12_model,
     *deepseek_v3_api_aliyun_model,
-    *deepseek_r1_api_aliyun_model,
+    *deepseek_r1_api_siliconflow_model,
     *lmdeploy_qwen2_5_7b_instruct_model,
     *lmdeploy_qwen2_5_72b_instruct_model,
     *lmdeploy_llama3_1_8b_instruct_model,
@@ -57,6 +50,18 @@ models = [
     # *lmdeploy_internlm3_8b_instruct_model,
     *lmdeploy_qwq_32b_preview_model,
 ]
+
+for model in models:
+    if model['abbr'].startswith('deepseek_r1_api_'):
+        model['return_reasoning_content'] = True
+        model['pred_postprocessor'] = {
+            'OpenHuEval_*': {'type': 'rm_<think>_before_eval'}
+        }
+    if model['abbr'].startswith('QwQ') and model['abbr'] != 'QwQ-32B-Preview':
+        model['pred_postprocessor'] = {
+            'OpenHuEval_*': {'type': 'rm_<think>_before_eval'}
+        }
+del model
 
 judge_models = [
     dict(
